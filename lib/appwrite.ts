d7c926912eea8868f423b34d6452b053b536b77e
Yasin Erkan <yasinerkan@gmail.com>
2025-06-15 01:01:02 +0200

    first comment

---
+++ lib/appwrite.ts
@@ -0,0 +1,31 @@
+import { Account, Client, Databases } from "react-native-appwrite";
+
+export const client = new Client()
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+
+  .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+
+export const DATABASE_ID = process.env.EXPO_PUBLIC_DB_ID!;
+
+export const HABITS_COLLECTION_ID =
+  process.env.EXPO_PUBLIC_HABITS_COLLECTION_ID!;
+
+export const COMPLETIONS_COLLECTION_ID =
+  process.env.EXPO_PUBLIC_COMPLETIONS_COLLECTION_ID!;
+
+export interface RealtimeResponse {
+  events: string[];
+  payload: any;
+}
+
+/*
+git clone https://github.com/appwrite/starter-for-react-native
+cd starter-for-react-native
+
+
+
+*/
